6585caf056fdad199d266e5276b2fde1a6a7588a
pukkamustard <pukkamustard@posteo.net>
2017-10-23 11:17:54 +0200

    Sum of neigbors using zippers

---
+++ sum.ml
@@ -0,0 +1,28 @@
+open BatPervasives
+
+
+let sum_of_neighbours = function
+  | Zipper.Zipper (hdl::_,x,hdr::_) -> hdl + x + hdr
+  | Zipper.Zipper ([], x, hdr::_) -> x + hdr
+  | Zipper.Zipper (hdl::_, x, []) -> hdl + x
+  | Zipper.Zipper ([], x, []) -> x
+
+
+let () =
+  let zipper = 
+    [ 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0]
+      |> Zipper.from_list
+      |> BatOption.default (Zipper.singleton 0)
+      |> ref
+  in
+    while true do
+      !zipper 
+        |> Zipper.to_list
+        |> List.map string_of_int
+        |> String.concat " "
+        |> print_endline;
+      zipper := !zipper
+                |> Zipper.extend sum_of_neighbours;
+      ignore @@ read_line () 
+    done
+
---
+++ zipper.ml
@@ -0,0 +1,57 @@
+open BatPervasives
+
+(** Comonadic Zipper *)
+
+(** The type of the zipper *)
+type 'a t =
+  | Zipper of  'a list * 'a * 'a list
+
+(** Shift left *)
+let shift_left = function
+  | Zipper (value :: left, old, right) -> Some (Zipper (left, value, old :: right))
+  | Zipper ([],_,_) -> None
+
+(** Shift right *)
+let shift_right = function
+  | Zipper (left, old, value :: right) -> Some (Zipper (old :: left, value, right))
+  | Zipper (_, _, []) -> None
+
+
+(* Comonad *)
+
+let map f = function
+  | Zipper (left, value, right) -> Zipper (List.map f left, f value, List.map f right)
+
+(* aka coreturn *)
+let extract = function
+  | Zipper (_, value, _) -> value
+
+(* aka cojoin *)
+let duplicate =
+  let
+    duplicate x =
+      (x , x)
+  in
+    function
+    | Zipper (left, value, right) as a -> 
+      Zipper (BatList.unfold a (shift_left %> BatOption.map duplicate)
+             , a
+             , BatList.unfold a (shift_right %> BatOption.map duplicate)
+             )
+(* aka cobind *)
+let extend f zipper =
+  map f (duplicate zipper)
+  
+
+(* Helpers *)
+
+(** Create zipper from List *)
+let from_list = function
+  | [] -> None
+  | hd :: tl -> Some (Zipper ([], hd, tl))
+
+let to_list = function
+  | Zipper (left, value, right) -> List.append (List.rev left) (value :: right)
+
+let singleton x =
+  Zipper ([], x, [])
